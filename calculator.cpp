f9eed5e36bb793fffa3586d97a02b92b23777ec7
Derek <dpauly84@gmail.com>
2015-04-14 17:01:55 -0500

    second

--- calculator.cpp
+++ calculator.cpp
@@ -6,64 +6,67 @@
 
 #include "calculator.hpp"
 #include "stack.hpp"
-#include <iostream>
-#include <sstream>
-#include <bits/algorithmfwd.h>
-#include <string>
-
-template<class Element>
-void print_stack(Stack<Element> e);
 
 using namespace std;
 
+template<class Element>
+void print_stack(Stack <Element> e);
+
 int main() {
 
-    string infix = "(3+(4*2)) /2";
-    string postfix;
+    string infix, postfix;
+    infix = "(4 - 2 + 6)";
 
     cout << "Enter an expression in infix notation: ";
-    cin >> infix;
+//    getline(cin, infix);
+
     postfix = infix_to_postfix(infix);
     cout << "Your expression converted to postfix is: " << postfix << endl;
     cout << endl;
 
     cout << "the result of evaluating your expression is: "
-         << compute_postfix(postfix);
+    << compute_postfix(postfix);
 
     return 0;
 } // End main
 
-string infix_to_postfix(string &infix) {
+string infix_to_postfix(string & infix) {
     string postfix = "";
     Stack<char> charStack;
-    char symbol;
-    char stack_sym;
-    for (int i = 0; i < infix.length(); ++i) {
+    char symbol; // character from string infix
+    char stack_sym; // character from top of stack
+    for (int i = 0; i < infix.length(); ++i) { // iterate through characters in infix string
         symbol = infix[i];
-        if (symbol == ' ') continue;
+        if (symbol == ' ') continue; // ignore spaces
         if (symbol == '(') charStack.push(symbol);
         else if (symbol == ')') {
             stack_sym = charStack.pop();
             while (stack_sym != '(') {
                 append_postfix(stack_sym, postfix);
+                if (charStack.is_empty()) break;
                 stack_sym = charStack.pop();
             }
         }
 
-        else if (!isalpha(symbol) && !isdigit(symbol)) {
-            if (charStack.is_empty()) charStack.push(symbol);
-            else if (charStack.top() == '(') charStack.push(symbol);
+        else if (!isalpha(symbol) && !isdigit(symbol)) { // symbol is an operator
+            if (charStack.is_empty()) {
+                charStack.push(symbol);
+            }
+            else if (charStack.top() == '(') {
+                charStack.push(symbol);
+            }
             else {
                 stack_sym = charStack.pop();
-                while (stack_sym != '(') {
+                while (stack_sym != '(' ) {
                     append_postfix(stack_sym, postfix);
+                    if (charStack.is_empty()) break;
                     stack_sym = charStack.pop();
                 }
-                charStack.push(stack_sym);
+                charStack.push(symbol);
             }
         }
 
-        else //* symbol is letter/number */
+        else // symbol is letter/number
         {
             append_postfix(symbol, postfix);
         }
@@ -77,8 +80,8 @@ string infix_to_postfix(string &infix) {
 }
 
 template<class Element>
-void print_stack(Stack<Element> e) {
-    list_element<Element> *ptr = new list_element<Element>;
+void print_stack(Stack <Element> e) {
+    list_element <Element> *ptr = new list_element <Element>;
     ptr = e.m_first;
     while (ptr != NULL) {
         cout << ptr->value << endl;
@@ -91,7 +94,7 @@ void append_postfix(char c, string &postfix) {
     postfix += " ";
 }
 
-double compute_postfix(string &postfix) {
+double compute_postfix(string & postfix) {
     Stack<double> doubleStack; // Initialize a stack of doubles
     double num1, num2;
     char symbol;
